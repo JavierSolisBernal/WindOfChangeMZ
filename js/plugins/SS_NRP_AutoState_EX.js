722fedbaad2dd6e1be4488fb074ee54a6231426f
Javier Solis Bernal <javier.solis@upgch.mx>
2025-11-22 11:05:53 -0600

    added conditions to states

--- js/plugins/SS_NRP_AutoState_EX.js
+++ js/plugins/SS_NRP_AutoState_EX.js
@@ -11,20 +11,28 @@
  * @help
  * It is plug and play
  * it determines if an autostate is applied
- * tags are <SS_state_cond>condition javascript here</SS_state_cond>
- * it must be resolve into a true false statement, if no valid it defaults to false
- * <SS_StatAbove: Stat1:x, Stat2:x>  
- * <SS_StatBelow: Stat1:x, Stat2:x>  
+ * State Notetags:
+ * 
+ * <SS_state_cond>condition javascript here</SS_state_cond>
+ * It must be resolve into a true false statement, if no valid it defaults to false
+ * 
+ * <SS_StatAbove: Stat1|x, Stat2|x>  
+ * <SS_StatBelow: Stat1|x, Stat2|x>  
  * Replace 'StatN' with 'HP', 'MP', 'TP', 'MAXHP', 'MAXMP', 'ATK', 'DEF',  'MAT', 'MDF', 'AGI', 'LUK'. 
  * If the above stat is above/below x, then the  condition is met for the passive state to appear.
- * add a % to make it compare with HP, MP, TP against the MaxHP, MaxMp, MaxTP
- * <SS_SwitchCondOn: 1,2,5>
- * the id of the switches that must be on to active for the passive state to appear
- * <SS_SwitchCondOff: 1,2,5>
- * the id of the switches that must be on to active for the passive state to appear
- * <SS_VarCond id: 1:==:test, 2:>=:20>
- * the comparison to variables format is id:operation:value 
- * valid operations: ==, ===, >, <, >=, <=, !=
+ * Add a % to make it compare with HP, MP, TP against the MaxHP, MaxMp, MaxTP in porcentual it is removed in the other stats
+ * Add a third parameter |true if want to include the equal
+ 
+ * <SS_SwitchOn: 1,2,5>
+ * The id of the switches that must be on to active for the passive state to appear
+ * <SS_SwitchOff: 1,2,5>
+ * The id of the switches that must be on to active for the passive state to appear
+ * <SS_VarAbove id 1|20|true, 2|10>
+ * Check if the variable is above a value
+ * Format is Var id|Value|flag where flag tells if the variable is also equal it is an optional parameter if empty the function is not equal
+ * <SS_VarBelow id 1|20|true, 2|10>
+ * Check if the variable is below a value
+ * Format is Var id|Value|flag where flag tells if the variable is also equal it is an optional parameter if empty the function is not equal
  * 
  * 
  */
@@ -80,10 +88,14 @@
         SS_autostates.list=[...result]
         SS_autostates.list.forEach((id) => {
             SS_autostates.list_cond[id]=SS_autostates.list_cond[id]||{}
-            SS_autostates.list_cond[id].switches=SS_autostates.list_cond[id].switches|| new Set()
-            SS_autostates.list_cond[id].vars=SS_autostates.list_cond[id].vars|| new Set()
+            SS_autostates.list_cond[id].switchesOn=SS_autostates.list_cond[id].switchesOn|| new Set()
+            SS_autostates.list_cond[id].switchesOff=SS_autostates.list_cond[id].switchesOff|| new Set()
+            SS_autostates.list_cond[id].varAbove=SS_autostates.list_cond[id].varAbove|| new Set()
+            SS_autostates.list_cond[id].varBelow=SS_autostates.list_cond[id].varBelow|| new Set()
+            SS_autostates.list_cond[id].statAbove=SS_autostates.list_cond[id].statAbove|| new Set()
+            SS_autostates.list_cond[id].statBelow=SS_autostates.list_cond[id].statBelow|| new Set()
 
-            let switchtest= $dataStates[id].meta.SS_SwitchCondOn || ""
+            let switchtest= $dataStates[id].meta.SS_SwitchOn || ""
             switchtest=makeArray(switchtest)
             switchtest=switchtest||[]
                      
@@ -93,15 +105,60 @@
             })
             SS_autostates.list_cond[id].switchesOn=[...SS_autostates.list_cond[id].switchesOn]  
 
-            let vartest= $dataStates[id].meta.SS_VarCond || ""
+
+            switchtest= $dataStates[id].meta.SS_SwitchOff || ""
+            switchtest=makeArray(switchtest)
+            switchtest=switchtest||[]
+                     
+            switchtest.forEach((v) => {
+                Number(v)
+                if (!isNaN(v)) SS_autostates.list_cond[id].switchesOff.add(v)
+            })
+            SS_autostates.list_cond[id].switchesOff=[...SS_autostates.list_cond[id].switchesOff]  
+
+            let vartest= $dataStates[id].meta.SS_VarAbove || ""
             vartest=makeArray(vartest)
             vartest=vartest||[]
                      
             vartest.forEach((v) => {
-                 SS_autostates.list_cond[id].vars.add(v)
+                 SS_autostates.list_cond[id].varAbove.add(v)
             })
-            SS_autostates.list_cond[id].vars=[...SS_autostates.list_cond[id].vars]  
+            SS_autostates.list_cond[id].varAbove=[...SS_autostates.list_cond[id].varAbove]  
 
+            vartest= $dataStates[id].meta.SS_VarBelow || ""
+            vartest=makeArray(vartest)
+            vartest=vartest||[]
+                     
+            vartest.forEach((v) => {
+                 SS_autostates.list_cond[id].varBelow.add(v)
+            })
+            SS_autostates.list_cond[id].varBelow=[...SS_autostates.list_cond[id].varBelow]  
+
+
+
+            let stattest= $dataStates[id].meta.SS_StatAbove || ""
+            stattest=makeArray(stattest)
+            stattest=stattest||[]
+                     
+            stattest.forEach((v) => {
+                 SS_autostates.list_cond[id].statAbove.add(v)
+            })
+            SS_autostates.list_cond[id].statAbove=[...SS_autostates.list_cond[id].statAbove]  
+ 
+            stattest= $dataStates[id].meta.SS_StatBelow || ""
+            stattest=makeArray(stattest)
+            stattest=stattest||[]
+                     
+            stattest.forEach((v) => {
+                 SS_autostates.list_cond[id].statBelow.add(v)
+            })
+            SS_autostates.list_cond[id].statBelow=[...SS_autostates.list_cond[id].statBelow]  
+ 
+
+            const note = $dataStates[id]?.note||""; // the full notebox string
+            const match = note.match(/<SS_state_cond>([\s\S]*?)<\/SS_state_cond>/i);
+            SS_autostates.list_cond[id].custom= match ? match[1].trim() : "";
+            eval (SS_autostates.list_cond[id].custom);
 
 
         })
